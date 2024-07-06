8f7284b954edf96e7b851fc4fcf86f406d022754
jaynnn <873242707@qq.com>
2024-07-06 17:30:00 +0800

    roulette

--- src/roulette.rs
+++ src/roulette.rs
@@ -1,33 +1,41 @@
 // 道具轮盘
 
-use bevy::prelude::*;
+use bevy::ecs::event;
+use bevy::{prelude::*, transform};
 
 use crate::player::Player;
-use crate::global::GlobalData;
+use crate::global::{self, GlobalData};
 
 pub fn roulette_plugin(app: &mut App) {
     app
-    
+        .register_type::<Roulette>()
+        .add_event::<RouletteRotateEvent>()
+        .add_event::<RouletteItemAddEvent>()
+        .add_systems(Startup, (
+            setup,
+        ))
         .add_systems(PreUpdate, (
             roulette_event,
         ))
         .add_systems(Update, (
-            setup,
-            update_roulette,
+            // update_roulette,
+            on_add_item,
+            test_add_item,
+            test_rotate,
         ));
 }
 
 // 轮盘物品
 #[derive(Component)]
 pub struct RouletteItem {
+    pub id: u32,
     pub name: String,
-    pub sprite_bundle: SpriteBundle,
 }
 
 // 轮盘
-#[derive(Component)]
+#[derive(Component, Reflect)]
 pub struct Roulette {
-    list: Vec<RouletteItem>,
+    list: Vec<Entity>,
     cur_index: usize,
 }
 
@@ -45,12 +53,32 @@ impl Roulette {
         Self::default()
     }
 
-    pub fn add_item(&mut self, item: RouletteItem) {
+    pub fn len(&self) -> usize {
+        self.list.len()
+    }
+
+    pub fn add_item(&mut self, item: Entity) {
         self.list.push(item);
     }
 
-    pub fn get_cur_item(&self) -> Option<&RouletteItem> {
+    pub fn get_item(&self, index: usize) -> Option<&Entity> {
+        self.list.get(index)
+    }
+
+    pub fn get_cur_index(&self) -> usize {
+        self.cur_index
+    }
+
+    pub fn get_cur_item(&self) -> Option<&Entity> {
         self.list.get(self.cur_index)
+    }
+
+    pub fn get_prev_item(&self) -> Option<&Entity> {
+        self.list.get((self.cur_index + self.list.len() - 1) % self.list.len())
+    }
+
+    pub fn get_next_item(&self) -> Option<&Entity> {
+        self.list.get((self.cur_index + 1) % self.list.len())
     }
 
     // 切换到下一个物品
@@ -69,11 +97,14 @@ fn setup(
 ) {
     let roulette = Roulette::new();
 
-    cmds.spawn((roulette,));
+    cmds.spawn((
+        roulette,
+        Name::new("Roulette")
+    ));
 }
 
 #[derive(Event)]
-pub enum RouletteEvent {
+pub enum RouletteRotateEvent {
     Left,
     Right
 }
@@ -81,35 +112,99 @@ pub enum RouletteEvent {
 // 轮盘事件, 切换物品
 // 用法：触发RouletteEvent事件
 pub fn roulette_event(
-    mut roulette_events: EventReader<RouletteEvent>,
+    mut roulette_events: EventReader<RouletteRotateEvent>,
+    mut query_roulette_item: Query<(&mut Transform, &mut RouletteItem)>,
     mut query: Query<&mut Roulette>,
+    global_data: Res<GlobalData>,
 ) {
     for event in roulette_events.read() {
         for mut roulette in query.iter_mut() {
             match event {
-                RouletteEvent::Left => {
+                RouletteRotateEvent::Left => {
                     roulette.prev();
                 }
-                RouletteEvent::Right => {
+                RouletteRotateEvent::Right => {
                     roulette.next();
                 }
+            }
+        }
+        
+        let roulette = query.single();
+        println!("cur_index: {} {}", roulette.get_cur_index(), roulette.len());
+        let cur_index = roulette.get_cur_index();
+        for i in 0..roulette.len() {
+            let item_id = roulette.get_item(i).unwrap();
+            if let Ok((mut transform, mut item)) = query_roulette_item.get_mut(*item_id) {
+                println!("======, {} {}", i, cur_index);
+                transform.translation.x = (i as f32 - cur_index as f32) * 50. * global_data.scale;
             }
         }
     }
 }
 
-pub fn update_roulette(
-    query_player: Query<&Transform, With<Player>>,
-    mut query_roulette: Query<(&Roulette, &mut Transform)>,
+pub fn test_rotate(
+    mut events: EventWriter<RouletteRotateEvent>,
+    input: Res<ButtonInput<KeyCode>>,
+) {
+    if input.just_pressed(KeyCode::KeyQ) {
+        events.send(RouletteRotateEvent::Left);
+    }
+    if input.just_pressed(KeyCode::KeyE) {
+        events.send(RouletteRotateEvent::Right);
+    }
+}
+
+#[derive(Event)]
+pub struct RouletteItemAddEvent {
+    pub id: u32,
+}
+
+// 添加物品
+pub fn on_add_item(
+    mut cmds: Commands,
+    mut roulette_item_add_events: EventReader<RouletteItemAddEvent>,
+    mut query_roulette: Query<&mut Roulette>,
+    query_player_e: Query<Entity, With<Player>>,
+    asset_server: Res<AssetServer>,
     global_data: Res<GlobalData>,
 ) {
-    let player_pos = query_player.single();
-    for (roulette, mut roulette_pos) in query_roulette.iter_mut() {
+    if let Ok(player) = query_player_e.get_single()
+    {
+        let mut roulette = query_roulette.single_mut();
+        for event in roulette_item_add_events.read() {
+            cmds.entity(player).with_children(|parent| {
+                let roulette_len = roulette.len();
+                let cur_index = roulette.get_cur_index();
+                let transform_x = (roulette_len - cur_index) as f32 * 50. * global_data.scale;
+                let id = parent.spawn((
+                    RouletteItem {
+                        id: event.id,
+                        name: "test".to_string(),
+                    }, SpriteBundle {
+                        sprite: Sprite {
+                            ..default()
+                        },
+                        texture: asset_server.load("images/player.png"),
+                        transform: Transform {
+                            translation: Vec3::new(transform_x, 50., 0.),
+                            ..default()
+                        },
+                        ..default()
+                    },
+                )).id();
+                roulette.add_item(id);
+            });
+        }
+    }
+}
 
-        let cur_roulette_pos = player_pos.translation.y + 200. * global_data.scale;
-        roulette_pos.translation.x = player_pos.translation.x;
-        roulette_pos.translation.y = cur_roulette_pos;
-
-        
+pub fn test_add_item(
+    mut events: EventWriter<RouletteItemAddEvent>,
+    input: Res<ButtonInput<KeyCode>>,
+) {
+    if input.just_pressed(KeyCode::KeyI) {
+        events.send(RouletteItemAddEvent {
+            id: 1,
+        });
     }
 }
